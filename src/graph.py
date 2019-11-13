9cd5c68c5bcfa6610955ea6f4bc7af2deed8d8c5
shiol <shiol@hotmail.com>
2019-11-13 00:02:22 -0300

    porcaria

--- src/graph.py
+++ src/graph.py
@@ -1,21 +1,28 @@
-# importing the required module 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
 
-# x axis values 
-x = [1,2,3] 
-# corresponding y axis values 
-y = [2,4,1] 
+# x axis values
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# corresponding y axis values
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# plotting the points 
-plt.plot(x, y) 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
 
-# naming the x axis 
-plt.xlabel('x - axis') 
-# naming the y axis 
-plt.ylabel('y - axis') 
 
-# giving a title to my graph 
-plt.title('My first graph!') 
+def animate(i):
+    list1[:] = [x + 1 for x in list1]
+    list2[:] = [x + 1 for x in list2]
+    ax.clear()
+    ax.plot(x, list1)
+    ax.plot(x, list2)
 
-# function to show the plot 
-plt.show() 
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
+
+# fig.title('stock values')
+# fig.xlabel('time')
+# fig.ylabel('value')
--- src/receive.py
+++ src/receive.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import pika
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -7,13 +10,52 @@ channel = connection.channel()
 
 channel.queue_declare(queue='apple')
 
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list1 = []
+list2 = []
+
+# ploting
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+
+def animate(i):
+    ax.clear()
+    if(len(list1) == 10 & len(list2) == 10):
+        ax.plot(x, list1)
+        ax.plot(x, list2)
+        print(list1)
+        print(list2)
+        list1.clear()
+        list2.clear()
+
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    if(method.routing_key == 'apple'):
+        aux = float(body)
+        list1.append(aux)
+        print('apple = %f' % aux)
+    elif (method.routing_key == 'microsoft'):
+        aux = float(body)
+        list2.append(aux)
+        print('ms = %f' % aux)
+    if(len(list1) == 10 & len(list2) == 10):
+        plt.plot(x, list1)
+        plt.plot(x, list2)
+        plt.draw()
+        plt.show()
+        print(list1)
+        print(list2)
+        list1.clear()
+        list2.clear()
 
 
 channel.basic_consume(
     queue='apple', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(
+    queue='microsoft', on_message_callback=callback, auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
+# ani = animation.FuncAnimation(fig, animate, repeat=False)
+# plt.show()
+
 channel.start_consuming()
